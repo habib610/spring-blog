6050a7e5d33cd26b140299a879fe062907212134
habib610 <habib54562@gmail.com>
2023-01-21 19:37:10 +0600

    router-setup

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,25 @@
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Navbar from "./components/global/Navbar";
-import Home from "./pages";
+import { HOME, LOGIN, REGISTRATION, WRITE } from "./constants/routes";
+import Home from "./pages/home";
+import Login from "./pages/login";
+import Registration from "./pages/registration";
+import Write from "./pages/write";
 function App() {
     return (
         <div className="App">
-            <BrowserRouter>
+            <Router>
                 <div>
                     <Navbar />
-                    <Home />
+                    <Routes>
+                        <Route path={HOME} element={<Home />} />
+                        <Route path={LOGIN} element={<Login />} />
+                        <Route path={REGISTRATION} element={<Registration />} />
+                        <Route path={WRITE} element={<Write />} />
+                        <Route path="*" element={<h1>404</h1>} />
+                    </Routes>
                 </div>
-            </BrowserRouter>
+            </Router>
         </div>
     );
 }
